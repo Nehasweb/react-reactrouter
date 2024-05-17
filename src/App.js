4afe4dc8059443a6bbe2e44e9336dc160ed70f43
Neha Gholap <nehagholap98@gmail.com>
2024-05-17 15:56:04 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      
+      <nav>
+        <Link to = '/'>Home</Link>
+        <Link to = '/about'>About</Link>
+        <Link to = '/contact'>Contact</Link>
+        <Link to = '/learn'>Learn</Link>
+
+      </nav>
+
+
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/about'element={<About/>}/>
+        <Route path='/contact'element={<Contact/>}/>
+        <Route path='/learn' />
+        <Route path=''element={<React/>}/>
+        <Route path='angular'element={<Angular/>}/>
+
+
+      </Routes>
+     </Router>
     </div>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Contact() {
+  return <h2>Contact</h2>
+}
+
+function React() {
+  return <h2>React</h2>
+}
+
+function Angular() {
+  return <h2>Angular</h2>
+}
+
 
 export default App;
